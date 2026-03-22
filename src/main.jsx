257d0c3131bc6faa10b045646d60c86435cca260
Manya Singh <manya426singh@gmail.com>
2026-03-22 17:11:21 +0530

    added interceptor

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,13 @@ import './index.css'
 // Set base URL for all axios calls globally
 axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
 
+// Set auth token on every request
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem('uv_token')
+  if (token) config.headers.Authorization = `Bearer ${token}`
+  return config
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
